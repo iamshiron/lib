3e990c40bcdd5e183176da4642d7ce46a3394df7
Shiron <justin.jakob391@outlook.com>
2025-11-19 03:37:58 +0100

    feat: Added profiler to sample project

--- samples/Shiron.Samples/src/Main.cs
+++ samples/Shiron.Samples/src/Main.cs
@@ -1,10 +1,23 @@
+using System.Security.AccessControl;
+using System.Security.Cryptography;
 using Shiron.Logging;
+using Shiron.Profiling;
 using Shiron.Samples.Logging;
 
 var logger = new Logger(null);
 _ = new LogRenderer(logger);
 
-logger.Info("This is an informational message.");
-logger.Warning("This is a warning message.");
-logger.Error("This is an error message.");
-logger.Debug("This is a debug message.");
+var profiler = new Profiler(logger, true);
+
+using (new ProfileScope(profiler, "Sample Scope")) {
+    logger.Info("This is an informational message.");
+    logger.Warning("This is a warning message.");
+    logger.Error("This is an error message.");
+    logger.Debug("This is a debug message.");
+}
+
+if (!Directory.Exists("profiles")) {
+    _ = Directory.CreateDirectory("profiles");
+}
+
+profiler.SaveToFile("profiles");
