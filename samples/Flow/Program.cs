9bc0d0adcd29576e4e49c2ddb771939b8e5a2578
Shiron <contact@shiron.io>
2026-02-12 19:12:44 +0100

    feat(flow): Introduce `Debouncer` utility with sample project

---
+++ samples/Flow/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using Shiron.Lib.Flow;
+
+var debouncer = new Debouncer(1750);
+
+while (true) {
+    Thread.Sleep(50);
+    if (debouncer.TryDebounce()) {
+        Console.WriteLine($"Debounce at {DateTimeOffset.UtcNow}");
+    }
+}
---
+++ src/Flow/Debouncer.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace Shiron.Lib.Flow;
+
+public class Debouncer {
+    private readonly long _interval;
+    private long _sinceLast;
+    
+    public Debouncer(long intervalMS) {
+        _interval = intervalMS * (Stopwatch.Frequency / 1000);
+    }
+
+    public void Reset() {
+        _sinceLast = 0;
+    }
+
+    public bool TryDebounce() {
+        var timePassed = Stopwatch.GetTimestamp() - _sinceLast;
+        if (timePassed >= _interval) {
+            _sinceLast = Stopwatch.GetTimestamp();
+            return true;
+        }
+        return false;
+    }
+}
