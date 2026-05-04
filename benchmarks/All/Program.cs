732f2d4f4089127c7a8beca631267a36b59439a4
Shiron <contact@shiron.io>
2026-05-04 20:43:24 +0200

    chore(pipeline): Removed test and benchmark projects
    Deleted due to upcoming frequent API changes that would make them brittle.

--- benchmarks/All/Program.cs
+++ benchmarks/All/Program.cs
@@ -1,11 +1,8 @@
-// See https://aka.ms/new-console-template for more information
-
 using BenchmarkDotNet.Running;
 using Shiron.Lib.Benchmarks.Collections;
 using Shiron.Lib.Benchmarks.Concurrency;
 using Shiron.Lib.Benchmarks.Flow;
 using Shiron.Lib.Benchmarks.Logging;
-using Shiron.Lib.Benchmarks.Pipeline;
 using Shiron.Lib.Benchmarks.Utils;
 
 Console.WriteLine("Hello, World!");
@@ -25,14 +22,6 @@ BenchmarkSwitcher.FromTypes([
 
     // FLow
     typeof(JobSchedulerBenchmarks),
-
-    // Pipeline
-    typeof(DagBenchmarks),
-    typeof(PipelineBuilderBenchmarks),
-    typeof(LinearChainExecutionBenchmarks),
-    typeof(WideFanOutExecutionBenchmarks),
-    typeof(ComplexGraphExecutionBenchmarks),
-    typeof(PipelineSerializationBenchmarks),
 
     // Utils
     typeof(FunctionUtilsBenchmarks),
